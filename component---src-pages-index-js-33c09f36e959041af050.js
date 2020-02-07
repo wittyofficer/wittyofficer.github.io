b086e5ab177b74a3015dfc4257dccf137f9bd54b
Prathamesh Vaste <pvaste@extron.com>
2020-02-06 16:02:39 -0800

    Updates

---
+++ component---src-pages-index-js-33c09f36e959041af050.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,o){"use strict";o.r(e);var t=o("mXGw"),w=o.n(t);e.default=function(){return w.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-33c09f36e959041af050.js.map
